52005fb1368b16696df9d4bd67f09f6488522a17
Hendrik Sattler <post@hendrik-sattler.de>
2008-03-20 00:23:22 +0000

    add get_peer function

--- src/net/inobex.c
+++ src/net/inobex.c
@@ -1,5 +1,8 @@
 #include "net.h"
 
+#include <arpa/inet.h>
+#include <errno.h>
+
 static
 obex_t* inet_init (
 	void* arg,
@@ -25,8 +28,56 @@ obex_t* inet_init (
 }
 
 static
+int inet_get_peer(
+	obex_t* handle,
+	char* buffer,
+	size_t bufsiz
+)
+{
+	struct sockaddr_storage addr;
+	socklen_t addrlen = sizeof(addr);
+	char addrstr[INET6_ADDRSTRLEN];
+	uint16_t port;
+	char tmp[256];
+
+	int status;
+	int sock = OBEX_GetFD(handle);
+
+	if (sock == -1)
+		return -EBADF;
+	status = getpeername(sock, (struct sockaddr*) &addr, &addrlen);
+	if (status == -1)
+		return -errno;
+
+	memset(addrstr, 0, sizeof(addrstr));
+	switch (((struct sockaddr_in*)&addr)->sin_family) {
+	case AF_INET:
+		if (inet_ntop(AF_INET, &((struct sockaddr_in*)&addr)->sin_addr, addrstr, sizeof(addrstr)) == NULL)
+			return -errno;
+		port = ((struct sockaddr_in*)&addr)->sin_port;
+		break;
+
+	case AF_INET6:
+		if (inet_ntop(AF_INET6, &((struct sockaddr_in6*)&addr)->sin6_addr, addrstr, sizeof(addrstr)) == NULL)
+			return -errno;
+		port = ((struct sockaddr_in6*)&addr)->sin6_port;
+		break;
+
+	default:
+		return -EBADF;
+	}
+	status = snprintf(tmp, sizeof(tmp), "tcp/[%s]:%u", addrstr, port);
+
+	if (buffer)
+		strncpy(buffer, tmp, bufsiz);
+
+	return status;
+}
+
+static
 struct net_funcs inet_funcs = {
-	.init = inet_init
+	.init = inet_init,
+	.get_peer = inet_get_peer
 };
 
 int inet_setup(struct net_data* data) {
