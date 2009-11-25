6989dae39f88f8fe020bee48b7a52862851dc08c
Hendrik Sattler <post@hendrik-sattler.de>
2009-11-25 18:03:44 +0100

    remove old code

--- src/action.h
+++ src/action.h
@@ -2,8 +2,5 @@
 
 void obex_action_connect (obex_t* handle, obex_object_t* obj, int event);
 void obex_action_disconnect (obex_t* handle, obex_object_t* obj, int event);
-
-void set_io_script (const char * script);
-
 void obex_action_put (obex_t* handle, obex_object_t* obj, int event);
 void obex_action_get (obex_t* handle, obex_object_t* obj, int event);
