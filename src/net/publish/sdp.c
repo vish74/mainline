87b6750ba30aa3ac35a1d4ab382cbaeb6bdd073e
Hendrik Sattler <post@hendrik-sattler.de>
2011-01-11 22:14:25 +0100

    Split SDP setup function, fix whitespace issues

--- src/net/publish/sdp.c
+++ src/net/publish/sdp.c
@@ -1,15 +1,15 @@
 /* Copyright (C) 2006 Hendrik Sattler <post@hendrik-sattler.de>
- *       
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.		       
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *	       
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
@@ -37,29 +37,56 @@ struct obex_sdp_data {
 };
 
 static
-int bt_sdp_fill_obex (struct obex_sdp_data *data, uint8_t channel)
+int bt_sdp_fill_l2cap (struct obex_sdp_data *data)
+{
+	data->list_l2cap = sdp_list_append(NULL, sdp_uuid16_create(&data->uuid_l2cap, L2CAP_UUID));
+	if (!data->list_l2cap)
+		return 0;
+
+	data->list_proto = sdp_list_append(NULL, data->list_l2cap);
+	if (!data->list_proto)
+		return 0;
+
+	return 1;
+}
+
+static
+int bt_sdp_fill_rfcomm (struct obex_sdp_data *data, uint8_t channel)
 {
 	data->chan = sdp_data_alloc(SDP_UINT8, &channel);
 	if (!data->chan)
 		return 0;
 
-	data->list_group  = sdp_list_append(NULL, sdp_uuid16_create(&data->uuid_group, PUBLIC_BROWSE_GROUP));
-	data->list_l2cap  = sdp_list_append(NULL, sdp_uuid16_create(&data->uuid_l2cap, L2CAP_UUID));
 	data->list_rfcomm = sdp_list_append(NULL, sdp_uuid16_create(&data->uuid_rfcomm, RFCOMM_UUID));
-	data->list_obex   = sdp_list_append(NULL, sdp_uuid16_create(&data->uuid_obex, OBEX_UUID));
-	if (!data->list_group || !data->list_l2cap || !data->list_rfcomm || !data->list_obex)
-		return 0;
-	
-	data->list_rfcomm = sdp_list_append(data->list_rfcomm, data->chan);
-	data->list_proto  = sdp_list_append(NULL, data->list_l2cap);
-	if (!data->list_rfcomm || !data->list_proto)
+	if (!data->list_rfcomm)
 		return 0;
 
-	data->list_proto  = sdp_list_append(data->list_proto, data->list_rfcomm);
+	data->list_rfcomm = sdp_list_append(data->list_rfcomm, data->chan);
+	if (!data->list_rfcomm)
+		return 0;
+
+	data->list_proto = sdp_list_append(data->list_proto, data->list_rfcomm);
 	if (!data->list_proto)
 		return 0;
 
-	data->list_proto  = sdp_list_append(data->list_proto, data->list_obex);
+	return 1;
+}
+
+static
+int bt_sdp_fill_obex (struct obex_sdp_data *data, uint8_t channel)
+{
+	data->list_group = sdp_list_append(NULL, sdp_uuid16_create(&data->uuid_group, PUBLIC_BROWSE_GROUP));
+	data->list_obex = sdp_list_append(NULL, sdp_uuid16_create(&data->uuid_obex, OBEX_UUID));
+	if (!data->list_group || !data->list_obex)
+		return 0;
+
+	if (!bt_sdp_fill_l2cap(data))
+		return 0;
+
+	if (!bt_sdp_fill_rfcomm(data, channel))
+		return 0;
+
+	data->list_proto = sdp_list_append(data->list_proto, data->list_obex);
 	if (!data->list_proto)
 		return 0;
 
@@ -170,7 +197,7 @@ void bt_sdp_cleanup_opp (struct obex_opp_sdp_data *data)
 		data->list_srv = NULL;
 	}
 	//data->list_formats?
-	bt_sdp_cleanup_obex(&data->obex);	
+	bt_sdp_cleanup_obex(&data->obex);
 }
 
 struct obex_ftp_sdp_data {
@@ -221,7 +248,7 @@ void bt_sdp_cleanup_ftp (struct obex_ftp_sdp_data *data)
 		sdp_list_free(data->list_srv, 0);
 		data->list_srv = NULL;
 	}
-	bt_sdp_cleanup_obex(&data->obex);	
+	bt_sdp_cleanup_obex(&data->obex);
 }
 
 #define SDP_DATA_REC_COUNT 2
@@ -258,7 +285,7 @@ struct sdp_data* bt_sdp (bdaddr_t* device, uint8_t channel, unsigned long protoc
 			return NULL;
 		}
 	}
-		
+
 	if ((protocols & BT_SDP_PROT_OBEX_FTP) != 0) {
 		data->rec[1].handle = sdp_record_alloc();
 		if (!data->rec[1].handle || !bt_sdp_fill_ftp(&data->rec[1].prot.ftp, channel, data->rec[1].handle)) {
@@ -308,7 +335,7 @@ void bt_sdp_session_close (
 
 	if (data->session == NULL)
 		return;
-	
+
 	for (int i = 0; i < SDP_DATA_REC_COUNT; ++i) {
 		if (data->rec[i].handle == NULL)
 			continue;
